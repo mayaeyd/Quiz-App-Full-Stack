1e01f95378864a430f07ecd2e36662aade7ccc19
Maya Eid <mayabaseleid@gmail.com>
2024-12-07 01:14:09 +0200

    Create authentication routes

--- quiz-app-server/src/routes/auth.routes.js
+++ quiz-app-server/src/routes/auth.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { login, register } from "../controllers/auth.controller";
 
 const router = Router();
 
-router.get();
-router.post();
+router.post("/login",login);
+router.post("/register", register);
+
+export default router;
