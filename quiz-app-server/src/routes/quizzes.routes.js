31f5e563a23e60bf8f4a0524c78873c88afff2f1
Maya Eid <mayabaseleid@gmail.com>
2024-12-07 14:04:52 +0200

    Create routes for quizzes and add necessary middleware to them

--- quiz-app-server/src/routes/quizzes.routes.js
+++ quiz-app-server/src/routes/quizzes.routes.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
+import {
+  createQuiz,
+  deleteQuiz,
+  getQuizzes,
+  updateQuiz,
+} from "../controllers/quizzes.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { adminMiddleware } from "../middlewares/admin.middleware";
 
 const router = Router();
 
-router.get();
-router.post();
-router.put();
-router.delete();
+router.get("/:id?", authMiddleware, getQuizzes);
+router.post("/", authMiddleware, adminMiddleware, createQuiz);
+router.put("/:id", authMiddleware, adminMiddleware, updateQuiz);
+router.delete("/:id", authMiddleware, adminMiddleware, deleteQuiz);
+
+export default router;
