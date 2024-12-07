1b1d462e6b1b220b9d10d7db660792799ff924b8
Maya Eid <mayabaseleid@gmail.com>
2024-12-07 15:09:32 +0200

    Handle login in the Login component

--- react-recap/src/pages/Login/index.jsx
+++ react-recap/src/pages/Login/index.jsx
@@ -1,12 +1,32 @@
 import { Button, Input } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { LoginUser } from "../../redux/slices/authSlice";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const { user, token, isLoggedIn, loading, error } = useSelector(
+    (state) => state.auth
+  );
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    dispatch(LoginUser({username, password}));
+  };
+
   return (
     <div>
-      <Input placeholder="username" />
-      <Input placeholder="password" />
-      <Button>Login</Button>
+      <Input
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={handleLogin}>Login</Button>
     </div>
   );
 };
