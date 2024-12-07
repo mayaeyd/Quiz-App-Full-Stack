0d2f281bf75ebccf48098c307ec27b575e59baef
Maya Eid <mayabaseleid@gmail.com>
2024-12-07 13:48:31 +0200

    Create getQuizzes controller

--- quiz-app-server/src/controllers/quizzes.controller.js
+++ quiz-app-server/src/controllers/quizzes.controller.js
@@ -1,12 +1,23 @@
-export const getQuizzes = async (req,res)=>{
-    try{
+import Quiz from "./../models/quiz.model.js";
 
-        
-
-    }catch(error){
-        console.log(error.message);
-        return res.status(500).send({
-            message:"Server error"
+export const getQuizzes = async (req, res) => {
+  try {
+    if (req.params.id) {
+      const quiz = await Quiz.findById(req.params.id);
+      if (!quiz) {
+        return res.status(404).send({
+          message: "Quiz not found",
         });
+      }
+      return res.status(200).send(quiz);
+    } else {
+      const quizzes = await Quiz.find();
+      return res.status(200).send(quizzes);
     }
-}
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({
+      message: "Server error",
+    });
+  }
+};
