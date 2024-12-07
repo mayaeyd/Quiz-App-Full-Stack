cfaef6af93848bdc52ac051a5a447421a9c3f8c6
Maya Eid <mayabaseleid@gmail.com>
2024-12-07 14:26:48 +0200

    Setup the authSlice on frontend

---
+++ react-recap/src/redux/slices/authSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  token: null,
+  isLoggedIn: false,
+  loading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+});
