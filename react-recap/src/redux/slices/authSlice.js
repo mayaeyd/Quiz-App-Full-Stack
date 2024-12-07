3116f5d248fc95aa053d109d5da3cee1a1e52a03
Maya Eid <mayabaseleid@gmail.com>
2024-12-07 15:07:13 +0200

    Fetch Login API in authSlice and test login

--- react-recap/src/redux/slices/authSlice.js
+++ react-recap/src/redux/slices/authSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   user: null,
@@ -8,8 +9,52 @@ const initialState = {
   error: null,
 };
 
+export const LoginUser = createAsyncThunk(
+  "login/LoginUser",
+  async (credentials) => {
+    try {
+      const { username, password } = credentials;
+      const response = await axios.post("http://127.0.0.1:8800/auth/login", {
+        username,
+        password,
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
-});
+  reducers: {
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      state.isLoggedIn = false;
+      localStorage.removeItem("token");
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(LoginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(LoginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        localStorage.setItem("token", action.payload.token);
+      })
+      .addCase(LoginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+}});
+
+export const {logout} = authSlice.actions;
+export default authSlice.reducer;
